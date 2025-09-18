b6108f1603e730ddb7d4525855c63ade99740677
Mara Dragomir <m.i.dragomir@student.vu.nl>
2025-09-18 11:23:19 +0200

    Attempting a new transformation but need to test at home

--- reverse_transformations.py
+++ reverse_transformations.py
@@ -19,14 +19,14 @@ def reshape_heart_patient(id_: str, path: Path) -> None:
 
     # THE MATRIX
         # APPARENLY REAL ORDER WAS T4 -> T3 -> R2 -> T1
-        # => REVERSE IS T1^-1 -> R2^-1 -> T1 -> T4^-1
+        # SO THE OG MATRIX WAS T1 @ R2 @ T3 @ T4
+        # TO REVERSE THAT, IT SHOULD BE T4^-1 @ T1 @ R2^-1 @ T1^-1
     fi = - (27 / 180) * np.pi
     T1: np.ndarray = np.array([[1, 0, 0, 275], [0, 1, 0, 200], [0, 0, 1, 0], [0, 0, 0, 1]])
     R2: np.ndarray = np.array([[np.cos(fi), -np.sin(fi), 0, 0], [np.sin(fi), np.cos(fi), 0, 0], \
         [0, 0, 1, 0], [0, 0, 0, 1]])
     T4: np.ndarray = np.array([[1, 0, 0, 50], [0, 1, 0, 40], [0, 0, 1, 15], [0, 0, 0, 1]])
-    #matrix: np.ndarray = np.linalg.inv(T1) @ np.linalg.inv(R2) @ T1 @ np.linalg.inv(T4)
-    matrix: np.ndarray = T4 @ np.linalg.inv(T1) @ R2 @ T1 
+    matrix: np.ndarray = np.linalg(T4) @ T1 @ np.linalg(R2) @ np.linalg(T1) 
 
     # TRANSFORMATION -> should it be per slice or can it be total?
     new_image: np.ndarray = scipy.ndimage.affine_transform(heart_mask, matrix, order=0) \
